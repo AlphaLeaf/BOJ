b551f12bd1eb76bcf90925912c57af95e8253b03
AlphaLeaf <24690794+AlphaLeaf@users.noreply.github.com>
2018-11-16 01:05:34 +0900

    https://www.acmicpc.net/problem/10172

---
+++ 10172_개.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	cout << "|\\_/|" << endl;
+	cout << "|q p|   /}" << endl;
+	cout << "( 0 )\"\"\"\\" << endl;//큰따옴표 앞에 백슬래시 넣어서 출력
+	cout << "|\"^\"`    |" << endl;
+	cout << "||_/=\\\\__|" << endl;
+	return 0;
+}
